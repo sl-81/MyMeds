a5220ec6c79232625878d27241a8f63a4660aeb7
sl-81 <s_li1992@hotmail.com>
2023-04-05 23:27:45 -0700

    Fixed Event and EventLog checkstyle

--- src/main/model/EventLog.java
+++ src/main/model/EventLog.java
@@ -10,18 +10,19 @@ import java.util.Iterator;
  * one EventLog in the system and that the system has global access
  * to the single instance of the EventLog.
  */
+
 public class EventLog implements Iterable<Event> {
 	/** the only EventLog in the system (Singleton Design Pattern) */
-	private static EventLog theLog;
-	private Collection<Event> events;
+    private static EventLog theLog;
+    private Collection<Event> events;
 	
 	/** 
 	 * Prevent external construction.
 	 * (Singleton Design Pattern).
 	 */
-	private EventLog() {
-		events = new ArrayList<Event>();
-	}
+    private EventLog() {
+        events = new ArrayList<Event>();
+    }
 	
 	/**
 	 * Gets instance of EventLog - creates it
@@ -29,31 +30,32 @@ public class EventLog implements Iterable<Event> {
 	 * (Singleton Design Pattern)
 	 * @return  instance of EventLog
 	 */
-	public static EventLog getInstance() {
-		if (theLog == null)
-			theLog = new EventLog();
-		
-		return theLog;
-	}
+    public static EventLog getInstance() {
+        if (theLog == null) {
+            theLog = new EventLog();
+        }
+
+        return theLog;
+    }
 	
 	/**
 	 * Adds an event to the event log.
 	 * @param e the event to be added
 	 */
-	public void logEvent(Event e) {
-		events.add(e);
-	}
+    public void logEvent(Event e) {
+        events.add(e);
+    }
 	
 	/**
 	 * Clears the event log and logs the event.
 	 */
-	public void clear() {
-		events.clear();
-		logEvent(new Event("Event log cleared."));
-	}
-	
-	@Override
+    public void clear() {
+        events.clear();
+        logEvent(new Event("Event log cleared."));
+    }
+
+    @Override
 	public Iterator<Event> iterator() {
-		return events.iterator();
-	}
+        return events.iterator();
+    }
 }
